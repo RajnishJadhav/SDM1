fd2b2f32e7ae0cda7648fad1e1276da35f9ba1a1
unknown <rajnishj0001@gmail.com>
2023-10-02 04:30:21 -0700

    Test

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-var express=require('express');
-var app=express();
+var express =require('express');
+var app = express();
 
 app.use(express.static('public'))
 
 
-
-app.get('/', (req, res) => {
+app.get("/",(req,res) => {
     res.sendFile("index.html")
-})
-  
-app.listen(8000, () => {
-    console.log(`Example app listening on port 8000`)
+    
+
+
+});
+app.listen(5000,() =>{
+    console.log('listning on port 5000');
 })
